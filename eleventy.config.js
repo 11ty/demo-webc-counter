135e083682fa53e3877f048e8b486a1bc53c308c
Zach Leatherman <zachleatherman@gmail.com>
2022-11-18 21:29:58 -0600

    Update to the newest versions

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,9 +3,7 @@ const pluginWebc = require("@11ty/eleventy-plugin-webc");
 module.exports = function(eleventyConfig) {
 	eleventyConfig.ignores.add("*.md");
 
-	eleventyConfig.addPlugin(pluginWebc, {
-		components: "_includes/webc/*.webc"
-	});
+	eleventyConfig.addPlugin(pluginWebc);
 
 	eleventyConfig.setServerPassthroughCopyBehavior("copy");
 	eleventyConfig.setServerOptions({
