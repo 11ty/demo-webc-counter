20d75c51b91e03612d5e84c0e885f4c909491ec7
Zearin <Zearin@users.noreply.github.com>
2023-05-04 16:39:56 -0400

    Add JSDoc (for completion) eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,6 @@
 const pluginWebc = require("@11ty/eleventy-plugin-webc");
 
+/** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
 module.exports = function(eleventyConfig) {
 	eleventyConfig.ignores.add("*.md");
 
